9634b02985a845a806a97dcdb2717b8b6b6f0b9a
Crusher <chris@zova.com>
2017-03-08 11:25:16 +1100

    Add tvOS target

---
+++ SwiftPhoenixClient-tvOS/SwiftPhoenixClient-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftPhoenixClient-tvOS.h
+//  SwiftPhoenixClient-tvOS
+//
+//  Created by Chris Fraser on 8/3/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftPhoenixClient-tvOS.
+FOUNDATION_EXPORT double SwiftPhoenixClient_tvOSVersionNumber;
+
+//! Project version string for SwiftPhoenixClient-tvOS.
+FOUNDATION_EXPORT const unsigned char SwiftPhoenixClient_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftPhoenixClient_tvOS/PublicHeader.h>
+
+
